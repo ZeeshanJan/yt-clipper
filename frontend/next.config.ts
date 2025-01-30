fcc037b65d835a372ccfc5f89a2827723e3eeb63
Zeeshan Jan <hello@zeeshan.uk>
2025-01-30 01:13:22 +0000

    updated config

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,23 @@
+/** @type {import('next').NextConfig} */
+
 import type { NextConfig } from "next";
+import path from "path";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  distDir: "dist",
+  basePath: "",
+  trailingSlash: true,
+  images: {
+    unoptimized: true,
+  },
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      "@": path.resolve(__dirname, "src"),
+    };
+    return config;
+  },
 };
 
 export default nextConfig;
